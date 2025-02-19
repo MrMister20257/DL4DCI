afaee2509a49731fb2057865a088e81117e22870
MrMister20257 <norman.a.marin@gmail.com>
2025-02-19 11:32:31 +0100

    added introInfo input check

--- functions/events/introInfo/introInfo.js
+++ functions/events/introInfo/introInfo.js
@@ -6,13 +6,34 @@ import loadingScreen from "../loadingScreen/loadingScreen.js";
 
 export default function introInfo() {
     loadingScreen();
-    player.Name = rs.question(`
+    const defaultName = "Max Mustermann";
+    let tempName = rs.question(`
 ${chalk.yellowBright("Wie nennet man dich?")}
 Dein Name: `);
 
-    player.maxDays = rs.question(`       
+    player.Name = tempName.trim()
+
+    if (player.Name.length === 0) {
+        player.Name = defaultName;
+    }
+
+    while(true) {
+        let tempMaxDays = parseInt(rs.question(`       
 ${chalk.yellowBright("Wie lange soll dein Abenteuer gehen?")}
-Max. Tage: `);
+Max. Tage: `), 10);
+
+        const hasLetters = /[a-zA-Z]/.test(tempMaxDays);
+        const hasSpezial = /[^a-zA-Z0-9]/.test(tempMaxDays);
+        
+        if (isNaN(tempMaxDays) || hasLetters || hasSpezial || tempMaxDays < 2) {
+            console.log(chalk.red(`Keine gültige Zahl. Bitte nur ganze Zahlen größer 1 eingeben!`));
+            continue;
+        }
+        
+        player.maxDays = tempMaxDays;
+            
+        break;
+    }
                                            
     return;
 };
